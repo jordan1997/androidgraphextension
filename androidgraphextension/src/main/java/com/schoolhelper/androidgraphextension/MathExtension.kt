118201bc261b86f988157326c66daa7d910da173
vova <v.stelmashchuk@mobidev.biz>
2018-09-03 00:23:59 +0300

    add extension for apache lib

---
+++ androidgraphextension/src/main/java/com/schoolhelper/androidgraphextension/MathExtension.kt
@@ -0,0 +1,12 @@
+package com.schoolhelper.androidgraphextension
+
+import android.graphics.PointF
+import org.apache.commons.math3.util.FastMath
+
+fun dim(a: PointF, b: PointF) = Math.sqrt(((a.x - b.x).sqr + (a.y - b.y).sqr).toDouble())
+
+val Float.sqr: Float
+	get() = this.toDouble().sqr.toFloat()
+
+val Double.sqr: Double
+	get() = FastMath.pow(this, 2.0)
