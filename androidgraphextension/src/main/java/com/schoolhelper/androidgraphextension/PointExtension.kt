05c02e46f18f8e8c68c6ac27e4be5c2c5f417b2c
vova <jordan29.04.1997@gmail.com>
2018-10-07 18:07:38 +0300

    fixed code

--- androidgraphextension/src/main/java/com/schoolhelper/androidgraphextension/PointExtension.kt
+++ androidgraphextension/src/main/java/com/schoolhelper/androidgraphextension/PointExtension.kt
@@ -4,32 +4,32 @@ import android.graphics.Point
 import android.graphics.PointF
 
 /**
-	Convert Point to PonitF;
-	@return PointF based on this instance
-*/
+	* Convert Point to PonitF;
+	* @return PointF based on this instance
+	*/
 fun Point.toPointF() = PointF(x.toFloat(), y.toFloat())
 
 /**
-	Return new Point object (x = originX * -1, y = originY * -1);
-*/
+	* Return new Point object (x = originX * -1, y = originY * -1);
+	*/
 operator fun Point.unaryMinus() = Point(-this.x, -this.y)
 
 /**
-	Minus one point from another point;
-	x = this.x - a
-	y = this.y - a
-	@return new instance of Point
-*/
+	* Minus one point from another point;
+	* x = this.x - a
+	* y = this.y - a
+	* @return new instance of Point
+	*/
 operator fun Point.minus(a: Float) = this.apply {
-	offset(a, a)
+	offset(a.toInt(), a.toInt())
 }
 
 /**
-	Plus point to this point, 
-	x = this.x + point.x
-	y = tihs.y + point.y
-	@return changed point;
-*/
+	* Plus point to this point,
+	* x = this.x + point.x
+	* y = tihs.y + point.y
+	* @return changed point;
+	*/
 operator fun Point.plus(point: Point) = this.apply {
 	offset(point.x, point.y)
 }
--- androidgraphextension/src/main/java/com/schoolhelper/androidgraphextension/Vector2DExtension.kt
+++ androidgraphextension/src/main/java/com/schoolhelper/androidgraphextension/Vector2DExtension.kt
@@ -5,18 +5,18 @@ import android.graphics.PointF
 import org.apache.commons.math3.geometry.euclidean.twod.Vector2D
 
 /**
-	Build a vector by two points
-	@param startPoint start of the vector
-	@param endPoint end of the vector
-	@return A instance of a (org.apach.commons.math3.geometry.exclidean.twod.Vector2D) vector
-*/
+	* Build a vector by two points
+	* @param startPoint start of the vector
+	* @param endPoint end of the vector
+	* @return A instance of a (org.apach.commons.math3.geometry.exclidean.twod.Vector2D) vector
+	*/
 fun Vector2D(startPoint: PointF, endPoint: PointF) = Vector2D((startPoint.x - endPoint.x).toDouble(), (startPoint.y - endPoint.y).toDouble())
 
 fun Vector2D(startPoint: Point, endPoint: Point) = Vector2D((startPoint.x - endPoint.x).toDouble(), (startPoint.y - endPoint.y).toDouble())
 
 /**
-	@return a vector which normal verctor for this. if this is a zero vector method returns a zero verctor
-*/
+	* @return a vector which normal verctor for this. if this is a zero vector method returns a zero verctor
+	*/
 fun Vector2D.safeNormalize() = (if (this == Vector2D.ZERO) {
 	Vector2D.ZERO
 } else {
@@ -24,6 +24,6 @@ fun Vector2D.safeNormalize() = (if (this == Vector2D.ZERO) {
 })!!
 
 /**
-	@return a normal vector for this
-*/
+	* @return a normal vector for this
+	*/
 fun Vector2D.makeNormalVector() = Vector2D(-this.y, this.x)
